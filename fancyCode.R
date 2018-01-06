d2e5ff392b287a791d87fdb55554c80b3a140eb7
Florian C.F. Schulz <florianCF.Schulz@gmail.com>
2018-01-06 21:36:08 +0100

    adding OLS function

--- fancyCode.R
+++ fancyCode.R
@@ -1,1 +1,29 @@
-#### Some fancy code here
+library(ggplot2)
+
+##### Loading the Data #####
+
+# Set working path
+path = "/home/flocf/Documents/git/SPL/"
+setwd(path)
+
+# Read in data
+WhiteWine = read.csv("data/winequality-white.csv", sep = ";")
+RedWine   = read.csv("data/winequality-red.csv", sep = ";")
+
+# Add column 'colour' and merge data.frames
+WhiteWine['colour'] = 'white'
+RedWine['colour']   = 'red'
+
+wines = rbind(WhiteWine, RedWine)
+
+##### Visualization of Data #####
+hist_qual = ggplot(wines, aes(quality, fill = colour)) +
+              geom_histogram(aes(y = ..density..), alpha = 0.5, position = 'identity', binwidth = 1) +
+              facet_wrap(~colour, nrow=2)
+
+hist_qual
+
+hist_alco = ggplot(wines, aes(alcohol, fill = colour)) +
+              geom_histogram(aes(y = ..density..), alpha = 0.5, position = 'identity', binwidth = 0.5)
+
+hist_alco
---
+++ functions/ols.R
@@ -0,0 +1,47 @@
+#######################################################################
+# OLS Schätzer with HC robust White Standard Errors ###################
+# Author: Florian Schulz ##############################################
+#######################################################################
+
+ols = function(formula, df, intercept = TRUE, var_method = "standard") {
+  # Transform formula into data frame (uses stats package)
+  data = model.frame(formula, df)
+  
+  # Define dependent variable
+  y = as.matrix(data[, 1])
+  # ... and independet variable
+  if (intercept) {
+    X = as.matrix(cbind(1, data[, -1]))
+    colnames(X)[1] = "(const)"
+  } else { X = as.matrix(data[, -1])}
+  
+  # Estimate beta based on standard formula (X'X)^-1 X'y
+  out = list(beta = as.numeric(solve(crossprod(X))%*%crossprod(X,y)))
+  
+  # Calculate residuals (res = y - X beta)
+  out$res = as.numeric(y - X%*%out$beta)
+  
+  # Calculate degrees of freedom
+  out$n  = nrow(X)
+  k      = ncol(X)
+  out$df = out$n - k
+  
+  # Calculate variance-covariance matrix
+  ## Maybe TODO: implement HAC estimators (kind of difficult, since needs weights)
+  if (var_method = "standard") {
+    out$Vp = 1/out$df * as.numeric(crossprod(out$res)) * solve(crossprod(X))
+  } else if (var_method  = "HC") {
+    out$Vp = solve(crossprod(X))%*%(crossprod(X, diag(as.numeric(out$res^2)))%*%X)%*%solve(crossprod(X))
+  } else {
+    warning("Use 'standard' or 'HC' as var_method, else 'standard' will be used.")
+    Vp = 1/out$df * as.numeric(crossprod(out$res)) * solve(crossprod(X))
+  }
+  
+  # Calculate standard errors of estimates
+  out$se = sqrt(diag(out$Vp))
+  
+  # Calculate p-values
+  out$p_value = 2*pt(abs(out$beta/out$se), df = out$df, lower.tail = FALSE)
+  
+  return(out)
+}
